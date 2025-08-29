b4dc1321d0469fa4484cd89009545c3e23349cba
alexsibtihon <alex.vl.tihon@gmail.com>
2025-08-29 19:24:58 +0700

    Add raw dumper

---
+++ internal/rawdumper/rawdumper.go
@@ -0,0 +1,110 @@
+package rawdumper
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/r2dtools/goapacheconf/internal/rawparser"
+)
+
+const (
+	tab   = "    "
+	space = " "
+)
+
+type RawDumper struct {
+	nestingLevel int
+}
+
+func (d *RawDumper) Dump(config *rawparser.Config) (string, error) {
+	if config == nil {
+		return "", errors.New("config is empty")
+	}
+
+	result := d.DumpEntries(config.Entries, false)
+
+	return result, nil
+}
+
+func (d *RawDumper) DumpEntries(entries []*rawparser.Entry, isInContainer bool) string {
+	var result string
+
+	for i, entry := range entries {
+		if entry != nil {
+			result += strings.Join(entry.StartNewLines, "")
+			result += d.DumpEntry(entry, isInContainer && i == 0)
+			result += strings.Join(entry.EndNewLines, "")
+		}
+	}
+
+	return result
+}
+
+func (d *RawDumper) DumpEntry(entry *rawparser.Entry, isFirstInContainer bool) string {
+	result := ""
+
+	if entry.BlockDirective != nil {
+		result += d.dumpBlockDirective(entry)
+	} else if entry.Directive != nil {
+		result += d.dumpDirective(entry)
+	} else if entry.Comment != nil {
+		result += d.dumpComment(entry, isFirstInContainer)
+	}
+
+	return result
+}
+
+func (d *RawDumper) dumpBlockDirective(entry *rawparser.Entry) string {
+	blockDirective := entry.BlockDirective
+	result := d.getCurrentIdent() + "<" + blockDirective.Identifier
+	parameters := strings.Join(blockDirective.GetParametersExpressions(), space)
+
+	if parameters != "" {
+		result += space + parameters
+	}
+
+	result += space + ">"
+
+	if blockDirective.Content != nil {
+		d.increaseNestingLevel()
+		result += d.DumpEntries(blockDirective.GetEntries(), true)
+		d.decreaseNestingLevel()
+	}
+
+	result += d.getCurrentIdent() + "</" + blockDirective.Identifier + ">"
+
+	return result
+}
+
+func (d *RawDumper) dumpDirective(entry *rawparser.Entry) string {
+	expression := strings.Join(entry.Directive.GetExpressions(), space)
+	identfier := d.getCurrentIdent() + entry.GetIdentifier()
+
+	if expression == "" {
+		return identfier
+	}
+
+	return identfier + space + expression
+}
+
+func (d *RawDumper) dumpComment(entry *rawparser.Entry, isFirstInContainer bool) string {
+	if isFirstInContainer && len(entry.StartNewLines) == 0 {
+		return space + entry.Comment.Value
+	}
+
+	return d.getCurrentIdent() + entry.Comment.Value
+}
+
+func (d *RawDumper) getCurrentIdent() string {
+	return strings.Repeat(tab, d.nestingLevel)
+}
+
+func (d *RawDumper) increaseNestingLevel() {
+	d.nestingLevel++
+}
+
+func (d *RawDumper) decreaseNestingLevel() {
+	if d.nestingLevel > 0 {
+		d.nestingLevel--
+	}
+}
--- internal/rawparser/rawparser.go
+++ internal/rawparser/rawparser.go
@@ -1,6 +1,8 @@
 package rawparser
 
 import (
+	"strings"
+
 	"github.com/alecthomas/participle/v2"
 	"github.com/alecthomas/participle/v2/lexer"
 )
@@ -12,7 +14,8 @@ const (
 	comment            = `(?:#)[^\n]*`
 	blockStartOpen     = `<`
 	blockStartClose    = `>`
-	blockEnd           = `</\s*` + ident + `\s*>`
+	blockEndOpen       = `<\/`
+	blockEndClose      = `>`
 	expression         = `[^#\s<>]+`
 	stringDoubleQuoted = `"(?:\\"|[^"])*"`
 	stringSingleQuoted = `'(?:\\'|[^'])*'`
@@ -42,7 +45,7 @@ type Directive struct {
 type BlockDirective struct {
 	Identifier string        `"<"@Ident`
 	Parameters []*Value      `@@*">"`
-	Content    *BlockContent `@@`
+	Content    *BlockContent `@@"</"`
 }
 
 type BlockContent struct {
@@ -51,6 +54,126 @@ type BlockContent struct {
 
 type Value struct {
 	Expression string `@Expression | @StringDoubleQuoted | @StringSingleQuoted`
+}
+
+func (c *Config) GetEntries() []*Entry {
+	entries := make([]*Entry, 0)
+
+	if c.Entries == nil {
+		return entries
+	}
+
+	return c.Entries
+}
+
+func (c *Config) SetEntries(entries []*Entry) {
+	c.Entries = entries
+}
+
+func (d *Directive) GetFirstValueStr() string {
+	if len(d.Values) == 0 {
+		return ""
+	}
+
+	return d.Values[0].Expression
+}
+
+func (d *Directive) GetExpressions() []string {
+	return getExpressions(d.Values)
+}
+
+func (d *Directive) GetValues() []*Value {
+	values := []*Value{}
+
+	for _, value := range d.Values {
+		if value != nil {
+			values = append(values, value)
+		}
+	}
+
+	return values
+}
+
+func (d *Directive) SetValues(expressions []string) {
+	values := []*Value{}
+
+	for _, expression := range expressions {
+		values = append(values, &Value{Expression: expression})
+	}
+
+	d.Values = values
+}
+
+func (b *BlockDirective) GetEntries() []*Entry {
+	entries := make([]*Entry, 0)
+
+	if b.Content == nil {
+		return entries
+	}
+
+	return b.Content.Entries
+}
+
+func (b *BlockDirective) FindEntriesWithIdentifier(identifier string) []*Entry {
+	entries := []*Entry{}
+
+	for _, entry := range b.GetEntries() {
+		if entry != nil && entry.GetIdentifier() == identifier {
+			entries = append(entries, entry)
+		}
+	}
+
+	return entries
+}
+
+func (b *BlockDirective) GetParametersExpressions() []string {
+	return getExpressions(b.Parameters)
+}
+
+func (b *BlockDirective) SetParameters(parameters []string) {
+	values := []*Value{}
+
+	for _, parameter := range parameters {
+		values = append(values, &Value{Expression: parameter})
+	}
+
+	b.Parameters = values
+}
+
+func (b *BlockDirective) SetEntries(entries []*Entry) {
+	if b.Content == nil {
+		b.Content = &BlockContent{}
+	}
+
+	b.Content.Entries = entries
+}
+
+func (b *BlockDirective) GetEntriesByIdentifier(identifier string) []*Entry {
+	entries := []*Entry{}
+
+	for _, entry := range b.GetEntries() {
+		if entry == nil {
+			continue
+		}
+
+		if strings.ToLower(entry.GetIdentifier()) == identifier {
+			entries = append(entries, entry)
+		}
+	}
+
+	return entries
+}
+
+func (e *Entry) GetIdentifier() string {
+	if e.Directive != nil {
+		return e.Directive.Identifier
+	}
+
+	if e.BlockDirective != nil {
+		return e.BlockDirective.Identifier
+	}
+
+	return ""
 }
 
 type RawParser struct {
@@ -97,8 +220,14 @@ func GetRawParser() (*RawParser, error) {
 			{Name: `NewLine`, Pattern: newLine, Action: nil},
 			{Name: `Comment`, Pattern: comment, Action: nil},
 			{Name: `Ident`, Pattern: ident, Action: lexer.Push("Directive")},
-			{Name: "blockEnd", Pattern: blockEnd, Action: lexer.Pop()},
+			{Name: "BlockEndOpen", Pattern: blockEndOpen, Action: lexer.Push("BlockEnd")},
 			{Name: "BlockStart", Pattern: blockStartOpen, Action: lexer.Push("BlockIdent")},
+			lexer.Return(),
+		},
+		"BlockEnd": {
+			{Name: `whitespace`, Pattern: whitespace, Action: nil},
+			{Name: `ident`, Pattern: ident, Action: nil},
+			{Name: `blockEndClose`, Pattern: blockEndClose, Action: lexer.Pop()},
 		},
 	})
 
@@ -116,4 +245,16 @@ func GetRawParser() (*RawParser, error) {
 	}
 
 	return &parser, nil
+}
+
+func getExpressions(values []*Value) []string {
+	expressions := []string{}
+
+	for _, value := range values {
+		if value != nil {
+			expressions = append(expressions, value.Expression)
+		}
+	}
+
+	return expressions
 }
